fbb2feed37732b1887e9157dd8f0b9feef680e61
Patrick Shriwise <p@gmail.com>
2017-10-16 14:16:00 -0500

    1

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,42 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+A=zeros(4,4)
+for i=1:4
+    for j=1:4
+        C=0.01*3^i
+        sigma=0.01*3^j
+        model=svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma))
+        predictions=svmPredict(model, Xval)
+        A(i,j)=mean(double(predictions ~= yval))
+    end
+end
+[M,I] = min(A(:))
+[I_row, I_col] = ind2sub(size(A),I)
+C=0.01*3^I_row
+sigma=0.01*3^I_col
+
+% =========================================================================
+
+end
